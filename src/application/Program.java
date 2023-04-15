0b6f360ad616d3769137ab901f678f5eaca2798f
Crislene Santos <28mariavalentina@gmail.com>
2023-04-15 16:28:52 -0300

    Chess layer and printing the board

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,14 +1,21 @@
 package application;
 
-import boardgame.Position;
+//import boardgame.Board;
+import chess.ChessMatch;
+//import boardgame.Position;
 
 public class Program {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		Position pos = new Position(3, 5);
-		System.out.println(pos);
+		//Position pos = new Position(3, 5); implantação do position aula 182
+		//System.out.println(pos);
+		
+		//Board board = new Board(8, 8);  implantacao piece e board aula 183
+		
+		ChessMatch chessMatch = new ChessMatch();
+		UI.printBoard(chessMatch.getPieces());
 	}
 
 }
