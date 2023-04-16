8b8d05f353776ba398c4fb1c47068e99ae373fd8
Crislene Santos <28mariavalentina@gmail.com>
2023-04-16 18:13:46 -0300

    Little improvement in board printing

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,7 +1,9 @@
 package application;
 
-//import boardgame.Board;
 import chess.ChessMatch;
+
+//import boardgame.Board;
+
 //import boardgame.Position;
 
 public class Program {
--- src/boardgame/Position.java
+++ src/boardgame/Position.java
@@ -5,11 +5,9 @@ public class Position {
 	private int column;
 	
 	public Position(int row, int column) {
-	
 		this.row = row;
 		this.column = column;
 	}
-	
 	public int getRow() {
 		return row;
 	}
@@ -26,5 +24,5 @@ public class Position {
 	public String toString() {
 		return row + ", " + column;
 	}
-	
+
 }
